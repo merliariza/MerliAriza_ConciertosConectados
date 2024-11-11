b03dc6674b441cc6b87d54ca52173c0d79af9b4e
Merli <180667185@users.noreply.github.com>
2024-11-11 13:50:11 -0500

    feat: :sparkles: nEW

--- js/notificaciones.js
+++ js/notificaciones.js
@@ -2,3 +2,14 @@ function toggleMenu() {
     var menu = document.getElementById('notificaciones-menu');
     menu.classList.toggle('show');
   }
+
+
+
+
+  window.addEventListener('focus', function () {
+    document.body.style.overflow = 'hidden'; // Impide que la pantalla se desplace cuando se enfoca el campo
+  });
+  
+  window.addEventListener('blur', function () {
+    document.body.style.overflow = ''; // Restaura el comportamiento cuando se pierde el enfoque
+  });
